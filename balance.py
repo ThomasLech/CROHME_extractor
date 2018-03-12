e32ce20401a6866820ebf63214b8b402994adda4
ThomasLech <itsawesome17@gmail.com>
2018-03-12 19:23:40 +0100

    Added "upper bound" option to balance.py script.

--- balance.py
+++ balance.py
@@ -8,7 +8,7 @@ import one_hot
 from random import shuffle
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 outputs_dir = 'outputs'
 train_out_dir = os.path.join(outputs_dir, 'train')
@@ -16,6 +16,7 @@ test_out_dir = os.path.join(outputs_dir, 'test')
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-b', '--box_size', required=True, help="Specify a length of square box side.")
+ap.add_argument('-ub', '--upper_bound', required=False, help="Specify the upper bound which essentially is the eventual distribution of each class after balance.")
 args = vars(ap.parse_args())
 
 box_size = int(args.get('box_size'))
@@ -33,7 +34,7 @@ print('Training set size:', len(train))
 print('Testing set size:', len(test))
 
 # Initialize keras image generator
-datagen = ImageDataGenerator(rotation_range=10, shear_range=0.2)
+datagen = ImageDataGenerator(rotation_range=10, shear_range=0.1)
 
 # Load all class_infos that were extracted
 classes = [label.strip() for label in list(open('classes.txt', 'r'))]
@@ -48,7 +49,11 @@ for train_sample in train:
 
 # Sort class_infos by occurrences
 class_infos = sorted(class_infos, key=lambda class_info: class_info['occurrences'], reverse=True)
-max_occurances = class_infos[0]['occurrences']
+if not args.get('upper_bound'):
+    max_occurances = class_infos[0]['occurrences']
+else:
+    max_occurances = int(args.get('upper_bound'))
+
 min_occurances = class_infos[len(class_infos)-1]['occurrences']
 for class_info in class_infos:
     class_info['deviation'] = max_occurances - class_info['occurrences']
@@ -74,14 +79,23 @@ for class_info in class_infos:
     # datagen.fit(images)
     new_data = []
     for X_batch, y_batch in datagen.flow(images, labels, batch_size=batch_size):
+        # # Plot newly generated images
+        # n_cols = 4
+        # n_rows = int(np.ceil(len(X_batch) / 4))
+        # figure, axis_arr = plt.subplots(n_rows, n_cols, figsize=(12, 4))
+        # for row in range(n_rows):
+        #     for col in range(n_cols):
+        #         axis_arr[row, col].imshow(X_batch[row*n_cols + col].reshape((box_size, box_size)), cmap='gray')
+        #         # Remove explicit axises
+        #         # axis_arr[row, col].axis('off')
+        # plt.show()
+
         # If enough samples were generated
         if len(new_data) >= how_many_gen:
             break;
         for idx in range(len(X_batch)):
             new_record = {'features': X_batch[idx].flatten(), 'label': y_batch[idx]}
             new_data.append(new_record)
-            # plt.imshow(X_batch[0].reshape((box_size, box_size)), cmap='gray')
-            # plt.show()
 
     print('CLASS: {}; NEW records: {};'.format(class_info['class'], len(new_data)))
     # Append newly generated data & shuffle given dataset
